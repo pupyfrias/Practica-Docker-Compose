fe9fc48850dd106936ca0a4ad96502b69b4ce540
Francisco Frias <fco.frias.11@gmail.com>
2024-06-19 15:47:26 -0400

    Proyect Added

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "mysql";
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+  die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa a la base de datos. Hola Mundo!";
+?>
